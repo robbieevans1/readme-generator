1b27eb51498f308ed644c40e46df9090495b695d
robcabobb <robcabobb@gmail.com>
2022-02-15 16:19:49 -0500

    add logic to generatemarkdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,78 @@
 // TODO: Include packages needed for this application
+const fs = require('fs')
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+			name: "title",
+			message: "What is your title of your project?",
+      validate: (projectNameInput) => {
+        if (projectNameInput) {
+          return true;
+        } else {
+          console.log("Please Enter your name!")
+          return false
+        }
+      }
+},
+  {
+    type: "input",
+			name: "description",
+			message: "Please describe your project:",
+      validate: (descriptionInput) => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log("A valid project description is required")
+          return false
+        }
+      }
+},
+{
+  type: "input",
+    name: "installation",
+    message: "Please describe the installtion process if applicable:",
+},
+{
+  type: "input",
+    name: "contribution",
+    message: "If applicable, provide guidelines on how others can contribute",
+},
+{
+  type: "list",
+    name: "license",
+    message: "Choose a license for your project:",
+    choices: ['MIT','GNU GPLv3','Apache License 2.0','ISC','N/A']
+},
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, error => {
+    if (error) {
+      return console.log('Sorry there was an unexpected error :' + error)
+    }
+
+    console.log("Success, your README.md file has been generated!")
+  })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions)
+  .then(function (newProject) {
+    console.log(newProject)
+    writeToFile("../dist/README.md", generateMarkdown(newProject))
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
 
 // Function call to initialize app
 init();
+
+module.exports = questions
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,82 @@
+const index = require("../index.js");
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
-
+let badge = "";
+if (license === "MIT") {
+	badge =
+		"![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)";
+} else if (license === "Apache 2.0") {
+	badge =
+		"![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)";
+} else if (license === "GPL v3.0") {
+	badge =
+		"![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)";
+} else {
+	badge = "";
+}
+return badge;
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink = '';
+    if(license === 'MIT') {
+      licenseLink = 'https://choosealicense.com/licenses/mit/'
+    } else if (license === 'Apache 2.0') {
+      licenseLink = 'http://www.apache.org/licenses/LICENSE-2.0'
+    } else if (license === 'GPL v3.0') {
+      licenseLink = 'https://www.gnu.org/licenses'
+    } else {
+      licenseLink = ""
+    }
+    return licenseLink;
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSection = ''
+  if(license === 'N/A') {
+    licenseSection = ''
+} else {
+  licenseSection =
+  `License: ${license} `
+}
+return licenseSection;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+	return `
+# ${data.projectName}
+
+## Project Description
+${data.description}
+
+## Table of Contents
+ *[Description](#description)
+ *[Installation Instructions](#installation)
+ *[Usage Guidelines](#usage)
+ *[Licensing](#license)
+ *[Testing](#tests)
+ *[Contribution](#contribution)
+ *[Questions/Concerns](#questions)
+
+ ## Installation
+${data.installInstructions}
+
+## Usage
+${data.usageInfo}
+
+### Licensing
+${data.installInstructions} 
+${renderLicenseSection(data)}
+
+## Contribution
+${data.contribGuidelines}
 
 `;
 }
+}
 
-module.exports = generateMarkdown;
+module.exports = generateMarkdown
